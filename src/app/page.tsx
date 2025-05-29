9b220faa1ff9d0804f85f5e02576dcc4f44434e4
MastyBoy <lukesatorius@gmail.com>
2025-05-29 16:05:09 -0700

    Remove duplicate FeedbackPanel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import CommandInput from "@/components/CommandInput";
-import FeedbackPanel from "@/components/FeedbackPanel";
 
 export default function Home() {
   return (
     <main className="min-h-screen p-8 space-y-8 bg-white">
       <CommandInput />
-      <FeedbackPanel />
     </main>
   );
 }
