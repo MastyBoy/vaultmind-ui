d6f39cc6daf3f7c0a3da129d6ddcf16fd326699a
MastyBoy <lukesatorius@gmail.com>
2025-05-29 00:26:26 -0700

    Fix: unify default export in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 import CommandInput from "@/components/CommandInput";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-white">
-      <CommandInput />
-    </main>
-  );
-}
-
 import FeedbackPanel from "@/components/FeedbackPanel";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-8 space-y-8">
+    <main className="min-h-screen p-8 space-y-8 bg-white">
       <CommandInput />
       <FeedbackPanel />
     </main>
